cc6d67d2a8bb4b4c9016e52ca21631d789207d4a
shadowrider38 <connorwang38@gmail.com>
2020-07-02 10:16:22 -0500

    Baseplate Tabs

--- Website Baseplate/app.js
+++ Website Baseplate/app.js
@@ -4,3 +4,22 @@ const links = document.querySelector(".links");
 navToggle.addEventListener('click', function () {
     links.classList.toggle('show-links');
 });
+
+const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
+
+about.addEventListener('click', function (e) {
+    const id = e.target.dataset.id;
+    if (id) {
+        btns.forEach(function (btn) {
+            btn.classList.remove('active');
+            e.target.classList.add('active');
+        });
+        articles.forEach(function (article) {
+            article.classList.remove('active');
+        });
+        const element = document.getElementById(id);
+        element.classList.add("active");
+    }
+});
